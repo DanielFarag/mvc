709c8a062447e9db6f75dec829b77f4a1c5f4ed7
Daniel Farag <danielfarag146@gmail.com>
2019-01-19 23:04:24 +0200

    Imporve Framework\Path class : Auto-retrieve directories

--- Framework/Path.php
+++ Framework/Path.php
@@ -2,15 +2,27 @@
 namespace Framework;
 
 class Path{
-	private static $main=['app','caches','config','database','framework','public','storage'];
+	private static $directories;
 	private function __construct(){}
 	private function __close(){}
 	
-	public static function __callStatic($dir,$params){
-		if(!in_array($dir,self::$main)){
+	public static function __callStatic($needle,$params){
+		if(empty(self::$directories)){
+			self::$directories = array_diff(scandir(APP_PATH),['.','..','.git']);
+		}
+		$found = false;
+		array_map(function($dir) use (&$needle,&$found){
+			if(is_dir(APP_PATH.DIRECTORY_SEPARATOR.$dir) && strtolower($needle) == strtolower($dir)){
+				$found = true;
+				$needle = $dir;
+			}
+		},self::$directories);
+		if(!$found){
 			throw new \Exception("Directory Not Found");
 		}
+		
 		$path=implode($params,DIRECTORY_SEPARATOR);
-		return APP_PATH.DIRECTORY_SEPARATOR.$dir.DIRECTORY_SEPARATOR.$path;
+		
+		return APP_PATH.DIRECTORY_SEPARATOR.$needle.DIRECTORY_SEPARATOR.$path;
 	}
 }
